54a6643b70fd3d11b4aa965e31759a14f695ddf2
Fabian-LS <fabianrluna11@gmail.com>
2022-08-11 02:50:22 +0000

    Respuesta del reto 1 de la sesion 7. Dentro de guthub de sesion 7

---
+++ S7_Reto1.R
@@ -0,0 +1,35 @@
+## RETO 01: RSTUDIO CLOUD -> GITHUB
+
+library(dplyr)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+# realizar la conexion con la base de datos
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+# mostrar las tablas dentro de la base de datos
+dbListTables(MyDataBase)
+
+# muestra los compos dentro de la tabla seleccionada
+dbListFields(MyDataBase, 'CountryLanguage')
+
+# realizar consulta tipo MySQL
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+names(DataDB)
+
+# consulta con filtro para los idiomas que se hablan
+library(dplyr)
+Lesp <- DataDB %>% filter(Language == "Spanish")
+Lesp
+Lesp.df <- as.data.frame(Lesp)
+
+library(ggplot2)
+Lesp.df %>% ggplot(aes( x = CountryCode, y=Percentage, fill = IsOfficial )) + 
+  geom_bar(stat = "identity")+
+  coord_flip()
